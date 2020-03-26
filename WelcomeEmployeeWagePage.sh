145c0febe7c20e3dcd0f1498d4c60a3174f22527
Mr. Avinash Rana <9771971429@users.noreply.github.com>
2020-03-26 14:34:18 +0530

    [avinash] added: added conditional block to check employee either persent or not

--- WelcomeEmployeeWagePage.sh
+++ WelcomeEmployeeWagePage.sh
@@ -2,3 +2,14 @@
 
 echo "Welcome to the Employee Wage Computation"
 
+#user case added to check employee persent or not
+
+isPersent = 1
+check_Persence = $((RANDOM%2))
+
+if (( $isPersent == $check_Persence ))
+then 
+    echo "Employee is persent"
+else 
+    echo "Employee is absent"
+fi
