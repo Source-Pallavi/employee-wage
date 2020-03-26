93856dd1bc9bd060d72c9f18967043b37dc3c6f3
Mr. Avinash Rana <9771971429@users.noreply.github.com>
2020-03-26 14:46:44 +0530

    [avinash] added: to calculate the total wage of an employee

--- WelcomeEmployeeWagePage.sh
+++ WelcomeEmployeeWagePage.sh
@@ -1,13 +1,27 @@
 #! /bin/bash
 
-# to calculate the total wage of an employee
+echo "Welcome to the Employee Wage Computation"
+
+#user case added to check employee persent or not
+
+isPersent = 1
+check_Persence = $((RANDOM%2))
 
 if (( $isPersent == $check_Persence ))
 then 
-    employee_Rate_Per_Hr=20;
-    working_Hrs=8;
-    salary=`expr $working_Hrs \* $employee_Rate_Per_Hr`
-else
-    salary=0;
+    echo "Employee is persent"
+else 
+    echo "Employee is absent"
+fi
+
+# to calculate total wage of employee
+
+if [ $isPresent -eq $randomCheck ]
+then 
+         employee_Rate_Per_Hr=20;
+         working_Hrs=8;
+         salary=`expr $working_Hrs \* $employee_Rate_Per_Hr`
+   else
+      salary=0;
 fi
 echo "Daily Wage of an Employee= " $salary
